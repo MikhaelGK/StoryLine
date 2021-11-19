71459c1457f0c34abb84f73141ee130e7625dc4e
Farazahwa <faazahfa@gmail.com>
2021-11-19 09:36:21 +0700

    KODE

---
+++ Entity.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Entity
+{
+    public class Entity
+    {
+        public int Health { get; set; }
+        public string Skill { get; set; }
+        public string Weapon { get; set; }
+        public string Inventory { get; set; }
+    }
+}
