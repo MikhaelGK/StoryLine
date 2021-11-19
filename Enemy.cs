b54257226be63229fc20d2892dbfa5e3e79179a8
Husniadyaksa <husniadyaksa112@gmail.com>
2021-11-19 10:39:07 +0700

    Hero

--- Enemy.cs
+++ Enemy.cs
@@ -9,7 +9,15 @@ namespace StoryLine
             Health = 20000;
             Skill = "Dragon Breath";            
         }
-
-        
+        public void RoyaleMinion()
+        {
+            Health = 500;
+            Skill = "Fair Ball";            
+        }
+        public void Goblin()
+        {
+            Health = 500;
+            Skill = "Thief";            
+        }        
     }
 }
